da0eda081c6fdecf6801bcbf8bd3e859730188ac
fcerfon <florent.cerfon@gmail.com>
2018-11-20 12:38:30 +0100

    ajout de fragments pour les questions, scores et settings

--- app/src/main/java/om/superquizz/diginamic/superquizz/MainActivity.java
+++ app/src/main/java/om/superquizz/diginamic/superquizz/MainActivity.java
@@ -3,6 +3,8 @@ package om.superquizz.diginamic.superquizz;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +17,10 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.content.Intent;
 
+import om.superquizz.diginamic.superquizz.ui.QuestionsFragment;
+import om.superquizz.diginamic.superquizz.ui.ScoreFragment;
+import om.superquizz.diginamic.superquizz.ui.SettingsFragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -24,24 +30,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
-        Button playButton = findViewById(R.id.playButton);
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, QuestionActivity.class);
-                startActivity(i);
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -90,6 +78,8 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = ((FragmentManager) fragmentManager).beginTransaction();
 
         if (id == R.id.nav_info) {
 
@@ -97,13 +87,26 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
 
             // Handle the camera action
-        }/* else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_gallery) {
+
+            QuestionsFragment fragment = new QuestionsFragment();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+            ScoreFragment fragment = new ScoreFragment();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_manage) {
+            SettingsFragment fragment = new SettingsFragment();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        } /*else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
